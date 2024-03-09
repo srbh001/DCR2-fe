e603cb2e8e52ecef90cf51ec6053655a62130406
Aayush Borkar <aayushb14@gmail.com>
2024-03-09 20:44:32 +0530

    Modify App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-import RegistrationPage from './pages/RegistrationPage';
+import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import LoginPage from "./pages/LoginPage";
+// import RegistrationPage from "./pages/RegistrationPage";
+import AppointmentsPage from "./pages/AppointmentsPage";
 
 export default function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/register" element={<RegistrationPage />} />
-        </Routes>
-      </Router>
+      {/* <Router> */}
+      {/*   <Routes> */}
+      {/*     <Route path="/" element={<LoginPage />} /> */}
+      {/*     <Route path="/register" element={<RegistrationPage />} /> */}
+      {/*   </Routes> */}
+      {/* </Router> */}
+      <AppointmentsPage />
     </div>
   );
 }
